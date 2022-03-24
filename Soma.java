04b2906f8a55ba0084a31ac98dda90bd53ed09b7
Vinícius Figueiredo Ferreira <93233422+vini12390@users.noreply.github.com>
2022-03-24 15:21:33 -0300

    Add files via upload

---
+++ Soma.java
@@ -0,0 +1,29 @@
+package projeto1;
+import java.util.Scanner;
+
+public class Soma {
+public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+	
+	
+	
+System.out.println("informe um número inteiro");
+
+int x = entrada.nextInt();
+
+System.out.println("informe outro número inteiro\n");
+
+int y = entrada.nextInt();
+
+int z =(x+y);
+
+
+
+System.out.println("a soma dos dois números é: "+z);
+
+
+
+
+
+}
+}
